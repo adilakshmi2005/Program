e4462cc3dd38b3ee5d37d14e80a55fe59e0b5b9a
adilakshmi2005 <chinnasamireddy1234@gmail.com>
2024-07-22 14:21:54 +0530

    Add files via upload

---
+++ Palindrome.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.palindrome;
+
+/**
+ *
+ * @author 91934
+ */
+public class Palindrome {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}

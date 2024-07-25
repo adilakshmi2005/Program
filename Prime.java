b7f26c2807b33fdfb24e739ede109b401514a935
adilakshmi2005 <chinnasamireddy1234@gmail.com>
2024-07-25 19:44:07 +0530

    Add files via upload

---
+++ Prime.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.prime;
+
+/**
+ *
+ * @author 91934
+ */
+public class Prime {
+
+    public static void main(String[] args) {
+        int a=4;
+        if(a%2==0)
+        {
+            System.out.println("even");
+        }
+        else
+        {
+            System.out.println("odd");
+        }
+    }
+}
